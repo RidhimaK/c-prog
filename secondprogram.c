8de635004a9e66b69effdffbf11bdfb36fa648d8
RidhimaK <49330919+RidhimaK@users.noreply.github.com>
2019-04-06 12:25:46 +0530

    Create secondprogram.c

---
+++ secondprogram.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void getinput(int *a,int *b)
+{
+     printf("Enter the value of first number");
+     scanf("%d",a);
+     printf("Enter the value of second number");
+     scanf("%d",b);
+
+}
+int addtn(int a ,int b)
+{
+     int c;
+     c=a+b;
+     return c;
+}
+void getoutput(int a,int b,int c)
+{
+     printf("%d+%d=%d",a,b,c);
+    
+
+}
+int main()
+{
+     int a,b,c;
+     getinput(&a,&b);
+     c=addtn(a,b)
+     ;
+     getoutput(a,b,c);
+}
